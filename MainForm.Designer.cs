8952a0debb3837ecac26b9afe145afdcdfcf637c
Zdeněk Gromnica <futuremillennium@gmail.com>
2024-05-22 17:15:08 +0200

    Compile Watchdog by Zdeněk Gromnica

---
+++ MainForm.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace CompileWatchdog {
+	partial class MainForm {
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing) {
+			if (disposing && (components != null)) {
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent() {
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+			this.fileSystemWatcher1 = new System.IO.FileSystemWatcher();
+			this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+			this.quitButton = new System.Windows.Forms.Button();
+			this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.lastOutputTextBox = new System.Windows.Forms.TextBox();
+			this.lastErrorTextBox = new System.Windows.Forms.TextBox();
+			this.compileCommandTextBox = new System.Windows.Forms.TextBox();
+			this.compileNowButton = new System.Windows.Forms.Button();
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).BeginInit();
+			this.groupBox1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// fileSystemWatcher1
+			// 
+			this.fileSystemWatcher1.EnableRaisingEvents = true;
+			this.fileSystemWatcher1.SynchronizingObject = this;
+			// 
+			// notifyIcon1
+			// 
+			this.notifyIcon1.Text = "Compile Watchdog";
+			this.notifyIcon1.Click += new System.EventHandler(this.notifyIcon1_Click);
+			// 
+			// quitButton
+			// 
+			this.quitButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.quitButton.Location = new System.Drawing.Point(639, 482);
+			this.quitButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.quitButton.Name = "quitButton";
+			this.quitButton.Size = new System.Drawing.Size(156, 57);
+			this.quitButton.TabIndex = 0;
+			this.quitButton.Text = "&Quit";
+			this.quitButton.UseVisualStyleBackColor = true;
+			this.quitButton.Click += new System.EventHandler(this.quitButton_Click);
+			// 
+			// checkedListBox1
+			// 
+			this.checkedListBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.checkedListBox1.FormattingEnabled = true;
+			this.checkedListBox1.Location = new System.Drawing.Point(13, 35);
+			this.checkedListBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.checkedListBox1.Name = "checkedListBox1";
+			this.checkedListBox1.Size = new System.Drawing.Size(368, 436);
+			this.checkedListBox1.TabIndex = 1;
+			this.checkedListBox1.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+			this.checkedListBox1.SelectedIndexChanged += new System.EventHandler(this.checkedListBox1_SelectedIndexChanged);
+			this.checkedListBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.checkedListBox1_KeyDown);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(13, 9);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(341, 21);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "Watched directories – drag a directory in to add:";
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.groupBox1.Controls.Add(this.lastOutputTextBox);
+			this.groupBox1.Controls.Add(this.lastErrorTextBox);
+			this.groupBox1.Controls.Add(this.compileCommandTextBox);
+			this.groupBox1.Location = new System.Drawing.Point(389, 35);
+			this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.groupBox1.Size = new System.Drawing.Size(406, 437);
+			this.groupBox1.TabIndex = 3;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "Compile command";
+			this.groupBox1.Visible = false;
+			// 
+			// lastOutputTextBox
+			// 
+			this.lastOutputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.lastOutputTextBox.Font = new System.Drawing.Font("Consolas", 14.25F);
+			this.lastOutputTextBox.Location = new System.Drawing.Point(8, 143);
+			this.lastOutputTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.lastOutputTextBox.Multiline = true;
+			this.lastOutputTextBox.Name = "lastOutputTextBox";
+			this.lastOutputTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.lastOutputTextBox.Size = new System.Drawing.Size(390, 143);
+			this.lastOutputTextBox.TabIndex = 2;
+			// 
+			// lastErrorTextBox
+			// 
+			this.lastErrorTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+			this.lastErrorTextBox.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lastErrorTextBox.Location = new System.Drawing.Point(8, 296);
+			this.lastErrorTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.lastErrorTextBox.Multiline = true;
+			this.lastErrorTextBox.Name = "lastErrorTextBox";
+			this.lastErrorTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.lastErrorTextBox.Size = new System.Drawing.Size(390, 131);
+			this.lastErrorTextBox.TabIndex = 1;
+			// 
+			// compileCommandTextBox
+			// 
+			this.compileCommandTextBox.Font = new System.Drawing.Font("Consolas", 14.25F);
+			this.compileCommandTextBox.Location = new System.Drawing.Point(9, 31);
+			this.compileCommandTextBox.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.compileCommandTextBox.Multiline = true;
+			this.compileCommandTextBox.Name = "compileCommandTextBox";
+			this.compileCommandTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+			this.compileCommandTextBox.Size = new System.Drawing.Size(389, 101);
+			this.compileCommandTextBox.TabIndex = 0;
+			this.compileCommandTextBox.TextChanged += new System.EventHandler(this.compileCommandTextBox_TextChanged);
+			// 
+			// compileNowButton
+			// 
+			this.compileNowButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.compileNowButton.Location = new System.Drawing.Point(13, 482);
+			this.compileNowButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.compileNowButton.Name = "compileNowButton";
+			this.compileNowButton.Size = new System.Drawing.Size(156, 57);
+			this.compileNowButton.TabIndex = 4;
+			this.compileNowButton.Text = "&Compile all now";
+			this.compileNowButton.UseVisualStyleBackColor = true;
+			this.compileNowButton.Click += new System.EventHandler(this.compileNowButton_Click);
+			// 
+			// MainForm
+			// 
+			this.AllowDrop = true;
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(808, 553);
+			this.Controls.Add(this.compileNowButton);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.checkedListBox1);
+			this.Controls.Add(this.quitButton);
+			this.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+			this.Name = "MainForm";
+			this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+			this.Text = "Compile Watchdog by Zdeněk Gromnica";
+			this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+			this.DragDrop += new System.Windows.Forms.DragEventHandler(this.MainForm_DragDrop);
+			this.DragOver += new System.Windows.Forms.DragEventHandler(this.MainForm_DragOver);
+			((System.ComponentModel.ISupportInitialize)(this.fileSystemWatcher1)).EndInit();
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.IO.FileSystemWatcher fileSystemWatcher1;
+		private System.Windows.Forms.NotifyIcon notifyIcon1;
+		private System.Windows.Forms.Button quitButton;
+		private System.Windows.Forms.CheckedListBox checkedListBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Button compileNowButton;
+		private System.Windows.Forms.TextBox compileCommandTextBox;
+		private System.Windows.Forms.TextBox lastOutputTextBox;
+		private System.Windows.Forms.TextBox lastErrorTextBox;
+	}
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CompileWatchdog {
+	public partial class MainForm : Form {
+		public List<WatchedDir> watchedDirs = new List<WatchedDir>();
+		public List<FileSystemWatcher> watchers = new List<FileSystemWatcher>();
+
+		public MainForm() {
+			InitializeComponent();
+			LoadSettings();
+			this.notifyIcon1.Icon = this.Icon;
+		}
+
+		private void MainForm_FormClosing(object sender, FormClosingEventArgs e) {
+			if (e.CloseReason == CloseReason.UserClosing) {
+				notifyIcon1.Visible = true;
+				this.Hide();
+				e.Cancel = true;
+			} else {
+				notifyIcon1.Visible = false;
+				SaveSettings();
+			}
+		}
+
+		private void quitButton_Click(object sender, EventArgs e) {
+			notifyIcon1.Visible = false;
+			Application.Exit();
+		}
+
+		private void MainForm_DragOver(object sender, DragEventArgs e) {
+			// accept if it's a folder
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+				string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+				if (files.Length == 1 && System.IO.Directory.Exists(files[0])) {
+					e.Effect = DragDropEffects.Copy;
+				}
+			}
+		}
+
+		private void notifyIcon1_Click(object sender, EventArgs e) {
+			this.Show();
+			notifyIcon1.Visible = false;
+		}
+
+		private void MainForm_DragDrop(object sender, DragEventArgs e) {
+			// accept if it's a folder, add to watchedDirs and checkedListBox1
+			if (e.Data.GetDataPresent(DataFormats.FileDrop)) {
+				string[] files = (string[])e.Data.GetData(DataFormats.FileDrop);
+				if (files.Length == 1 && System.IO.Directory.Exists(files[0])) {
+					WatchedDir wd = new WatchedDir();
+					wd.path = files[0];
+					wd.enabled = true;
+					//watchedDirs.Add(wd);
+					//// add and check
+					//checkedListBox1.Items.Add(wd.path, true);
+
+					Add(wd);
+
+					//checkedListBox1.SetItemChecked(checkedListBox1.Items.Count - 1, true);
+					//RefreshWatchedDirs();
+				}
+			}
+		}
+
+		private void compileCommandTextBox_TextChanged(object sender, EventArgs e) {
+			if (checkedListBox1.SelectedIndex >= 0) {
+				watchedDirs[checkedListBox1.SelectedIndex].compileCommand = compileCommandTextBox.Text;
+			}
+		}
+
+		private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e) {
+			if (checkedListBox1.SelectedIndex >= 0) {
+				groupBox1.Text = watchedDirs[checkedListBox1.SelectedIndex].path;
+				compileCommandTextBox.Text = watchedDirs[checkedListBox1.SelectedIndex].compileCommand;
+				groupBox1.Visible = true;
+			} else {
+				groupBox1.Visible = false;
+			}
+		}
+
+		private void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e) {
+			if (e.NewValue == CheckState.Unchecked) {
+				watchedDirs[e.Index].enabled = false;
+			} else {
+				watchedDirs[e.Index].enabled = true;
+			}
+		}
+
+		private void compileNowButton_Click(object sender, EventArgs e) {
+			// compile all checked dirs
+			foreach (WatchedDir wd in watchedDirs) {
+				if (wd.enabled) {
+					HandleCompile(wd);
+				}
+			}
+		}
+
+		void Add(WatchedDir wd, bool addToWD = true) {
+			if (addToWD) {
+				watchedDirs.Add(wd);
+			}
+			checkedListBox1.Items.Add(wd.path, wd.enabled);
+
+			// add a watcher
+			FileSystemWatcher watcher = new FileSystemWatcher();
+			watcher.Path = wd.path;
+			watcher.NotifyFilter = NotifyFilters.LastWrite;
+			watcher.Filter = "*.*";
+			watcher.IncludeSubdirectories = true;
+			watcher.Changed += new FileSystemEventHandler(OnChanged);
+			watcher.EnableRaisingEvents = true;
+			watchers.Add(watcher);
+		}
+
+		void HandleCompile(WatchedDir wd) {
+			Compile(wd);
+			if (wd.lastError.Length > 0) {
+				if (this.InvokeRequired) {
+					this.Invoke(new Action(() => {
+						RefocusRefresh(wd);
+					}));
+				} else {
+					RefocusRefresh(wd);
+				}
+			}
+		}
+
+		void RefocusRefresh(WatchedDir wd) {
+			if (checkedListBox1.SelectedIndex >= 0) {
+				lastOutputTextBox.Text = watchedDirs[checkedListBox1.SelectedIndex].lastOutput;
+				lastErrorTextBox.Text = watchedDirs[checkedListBox1.SelectedIndex].lastError;
+			}
+			this.Show();
+			this.Focus();
+		}
+
+		void Compile(WatchedDir wd) {
+			// run compile command
+			System.Diagnostics.ProcessStartInfo psi = new System.Diagnostics.ProcessStartInfo();
+			psi.FileName = "cmd.exe";
+			psi.Arguments = "/c " + wd.compileCommand;
+			psi.WorkingDirectory = wd.path;
+			psi.UseShellExecute = false;
+			psi.CreateNoWindow = true;
+			psi.RedirectStandardOutput = true;
+			psi.RedirectStandardError = true;
+			System.Diagnostics.Process p = System.Diagnostics.Process.Start(psi);
+			p.WaitForExit();
+			string output = p.StandardOutput.ReadToEnd();
+			string error = p.StandardError.ReadToEnd();
+			wd.lastOutput = output;
+			wd.lastError = error;
+			/*if (output.Length > 0) {
+				MessageBox.Show(output);
+			}
+			if (error.Length > 0) {
+				MessageBox.Show(error);
+			}*/
+		}
+
+		void SaveSettings() {
+			// save watchedDirs to settings.xml
+			System.Xml.Serialization.XmlSerializer xs = new System.Xml.Serialization.XmlSerializer(typeof(List<WatchedDir>));
+			System.IO.Stream s = new System.IO.FileStream("settings.xml", System.IO.FileMode.Create);
+			xs.Serialize(s, watchedDirs);
+			s.Close();
+		}
+
+		void LoadSettings() {
+			// load watchedDirs from settings.xml
+			if (System.IO.File.Exists("settings.xml")) {
+				System.Xml.Serialization.XmlSerializer xs = new System.Xml.Serialization.XmlSerializer(typeof(List<WatchedDir>));
+				System.IO.Stream s = new System.IO.FileStream("settings.xml", System.IO.FileMode.Open);
+				watchedDirs = (List<WatchedDir>)xs.Deserialize(s);
+				s.Close();
+				foreach (WatchedDir wd in watchedDirs) {
+					Add(wd, false);
+					//checkedListBox1.Items.Add(wd.path, wd.enabled);
+					wd.justFired = false;
+				}
+			}
+		
+		}
+
+		private void checkedListBox1_KeyDown(object sender, KeyEventArgs e) {
+			// delete on [del]
+			if (e.KeyCode == Keys.Delete) {
+				if (checkedListBox1.SelectedIndex >= 0) {
+					watchedDirs.RemoveAt(checkedListBox1.SelectedIndex);
+					checkedListBox1.Items.RemoveAt(checkedListBox1.SelectedIndex);
+				}
+			}
+		}
+
+		private void OnChanged(object source, FileSystemEventArgs e) {
+			//MessageBox.Show("File: " + e.FullPath + " " + e.ChangeType);
+			// compile
+			foreach (WatchedDir wd in watchedDirs) {
+				if (e.FullPath.StartsWith(wd.path)) {
+					if (wd.justFired) { // Assuming the event fires twice because of Windows behaviour.
+						wd.justFired = false;
+					} else {
+						HandleCompile(wd);
+						wd.justFired = true;
+					}
+				}
+			}
+		}
+	}
+
+	public class WatchedDir {
+		public string path;
+		public bool enabled;
+		public string compileCommand;
+		public string lastOutput;
+		public string lastError;
+		public bool justFired = false;
+	}
+}
